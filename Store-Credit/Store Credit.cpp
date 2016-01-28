86ac73051073f134650cdb8b2d851ed0d7e53cbc
xinranzheng <xz1021@nyu.edu>
2016-01-27 20:08:59 -0500

    finishing the code and fixed all the known errors

--- Store-Credit/Store Credit.cpp
+++ Store-Credit/Store Credit.cpp
@@ -14,13 +14,13 @@ For each test case, output one line containing "Case #x: " followed by the indic
 credit. The lower index should be output first.
 */
 
-#include <iostram>
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <unordered_map>
-using namspace std;
+using namespace std;
 
-void buying(const vector<int>& prices, const unordered_map<int, int>& copy, int money, int case) {
+void buying(ofstream& output, const vector<int>& prices, const unordered_map<int, int>& copy, int money, int aCase) {
 	//go through vector, for each item, check hash table to see if there's a complement
 	//if there's a complement, output result and break out of loop
 	//else keep going through the vector
@@ -30,18 +30,18 @@ void buying(const vector<int>& prices, const unordered_map<int, int>& copy, int 
 		unordered_map<int, int>::const_iterator item = copy.find(money - prices[i]);
 		if (item != copy.end()) {
 			if (i < item->second) {
-				cout << "Case #" << case << ": " << i << " " << item->second << endl;
+				output << "Case #" << aCase << ": " << i + 1 << " " << item->second + 1 << endl;
 			}
 			else {
-				cout << "Case #" << case << ": " << item->second << " " << i << endl;
+				output << "Case #" << aCase << ": " << item->second + 1 << " " << i + 1 << endl;
 			}
-			break;
+			return;
 		}
 	}
-	cerr << "Something's wrong, my frand";
+	cerr << "Something's wrong, my frand" << endl;
 }
 
-void inventory(ifstream& input) {
+void inventory(ifstream& input, ofstream& output) {
 	//for every case, get money amount and put all the items in a vector and a hash table
 	//call buying to figure out which 2 items add up to the money given
 	vector<int> prices;
@@ -50,25 +50,30 @@ void inventory(ifstream& input) {
 	int money;
 	int numItem;
 	int item;
-	int vector<int> items;
 	input >> cases;
 	for (int i = 1; i <= cases; i++) {
 		input >> money >> numItem;
 		for (int j = 0; j < numItem; j++) {
 			input >> item;
-			items.push_back(item);
+			prices.push_back(item);
 			copy[item] = j; //item becomes the key, and the value is the index in the vector
 		}
-		buying(prices, copy, money, i);
+		buying(output, prices, copy, money, i);
+		prices.clear();
+		copy.clear();
 	}
 }
 
 int main() {
 	ifstream input;
-	input.open("A-small-practice.in");
+	input.open("A-large-practice.in");
 	if (!input) {
 		cerr << "Cannot open file";
 		exit(0);
 	}
-	inventory(input);
+	ofstream output;
+	output.open("output.txt");
+	inventory(input, output);
+	input.close();
+	output.close();
 }
